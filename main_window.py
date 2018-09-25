f23c96d9117881b9a53e86f54fb8a70a965becfc
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2018-09-25 16:11:45 +0400

    Still getting into PyQt stuff...

--- main_window.py
+++ main_window.py
@@ -1,10 +1,12 @@
 import json
+import os
 import sys
+from threading import Thread
 
 from PyQt5 import QtWidgets
 
-import main_window_design
 import build_manager as build_mng
+import main_window_design
 
 
 class ExampleApp(QtWidgets.QMainWindow, main_window_design.Ui_MainWindow):
@@ -15,13 +17,21 @@ class ExampleApp(QtWidgets.QMainWindow, main_window_design.Ui_MainWindow):
         self.actionSetRootFolder.triggered.connect(self.browse_folder)
         self.actionQuit.triggered.connect(lambda: sys.exit())
         self.btnUpdate.clicked.connect(self.update)
+        self.btnCancel.clicked.connect(self.cancel)
+        self.btnOpenRootFolder.clicked.connect(self.open_root_folder)
         self.btnCancel.hide()
 
+        root_path = self.read_settings('root_path')
+        self.labelRootPath.setText(
+            root_path if root_path else "No Root Folder Specified!")
+
     def browse_folder(self):
-        dir = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        dir = QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Выберите папку")
 
         if dir:
-            self.write_settings('root_folder', dir)
+            self.write_settings('root_path', dir)
+            self.labelRootPath.setText(dir)
 
     def read_settings(self, key):
         with open('settings.txt', 'r') as settings_file:
@@ -45,8 +55,19 @@ class ExampleApp(QtWidgets.QMainWindow, main_window_design.Ui_MainWindow):
         root_path = self.read_settings('root_path')
         self.btnUpdate.hide()
         self.btnCancel.show()
-        build_mng.load_zip(self.progressBar, root_path)
+        self.t = Thread(target = build_mng.load_zip(self.progressBar, root_path))
+
+        self.btnCancel.hide()
         self.btnUpdate.show()
+
+    def cancel(self):
+        self.t._stop()
+
+    def open_root_folder(self):
+        root_path = self.read_settings('root_path')
+
+        if root_path:
+            os.startfile(root_path)
 
 
 def main():
