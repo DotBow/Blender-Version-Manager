8d2ddd0a09b645ccbfa0c61e475b1e44c706585b
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2019-02-17 01:11:56 +0400

    Use QProcess for running blender. Use dynamic properties in style.

--- version_layout.py
+++ version_layout.py
@@ -1,12 +1,10 @@
 import asyncio
 import os
 import shutil
-import subprocess
 import threading
 import time
 
-import psutil
-from PyQt5 import QtCore
+from PyQt5.QtCore import QProcess, Qt
 from PyQt5.QtGui import QCursor, QFont, QIcon, QPixmap
 from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QMessageBox,
                              QPushButton, QSizePolicy)
@@ -17,92 +15,97 @@ import resources_rc
 class B3dItemLayout(QHBoxLayout):
     def __init__(self, root_folder, version, is_latest, parent):
         super(B3dItemLayout, self).__init__(None)
+
         self.btn_open_style = \
-            ("""QPushButton
-                    {
-                        color: rgb(255, 255, 255);
-                        background-color: rgb(51, 51, 51);
-                        border-style: solid;
-                        border-color: rgb(51, 51, 51);
-                        border-width: 6px;
-                    }
+            ("""QPushButton[IsRunning=false]
+                {
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(51, 51, 51);
+                    border-style: solid;
+                    border-color: rgb(51, 51, 51);
+                    border-width: 6px;
+                }
 
-                    QPushButton:pressed
-                    {
-                        background-color: rgb(80, 80, 80);
-                        border-color: rgb(80, 80, 80);
-                    }
+                QPushButton[IsRunning=false]:pressed
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }
 
-                    QPushButton:hover
-                    {
-                        background-color: rgb(80, 80, 80);
-                        border-color: rgb(80, 80, 80);
-                    }""")
+                QPushButton[IsRunning=false]:hover
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }
+                
+                QPushButton[IsRunning=true]
+                {
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(204, 102, 51);
+                    border-style: solid;
+                    border-color: rgb(204, 102, 51);
+                    border-width: 6px;
+                }
+
+                QPushButton[IsRunning=true]:pressed
+                {
+                    background-color: rgb(204, 102, 51);
+                    border-color: rgb(204, 102, 51);
+                }
+
+                QPushButton[IsRunning=true]:hover
+                {
+                    background-color: rgb(204, 102, 51);
+                    border-color: rgb(204, 102, 51);
+                }""")
 
         self.btn_delete_style = \
-            ("""QPushButton
-                    {
-                        color: rgb(255, 255, 255);
-                        background-color: rgb(51, 51, 51);
-                        border-style: solid;
-                        border-color: rgb(51, 51, 51);
-                        border-width: 0px 4px 0px 4px;
-                        qproperty-icon: url(:/icons/delete.svg);
-                        qproperty-iconSize: 20px;
-                    }
+            ("""QPushButton[IsRunning=false]
+                {
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(51, 51, 51);
+                    border-style: solid;
+                    border-color: rgb(51, 51, 51);
+                    border-width: 0px 4px 0px 4px;
+                    qproperty-icon: url(:/icons/delete.svg);
+                    qproperty-iconSize: 20px;
+                }
 
-                    QPushButton:pressed
-                    {
-                        background-color: rgb(80, 80, 80);
-                        border-color: rgb(80, 80, 80);
-                    }
+                QPushButton[IsRunning=false]:pressed
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }
 
-                    QPushButton:hover
-                    {
-                        background-color: rgb(80, 80, 80);
-                        border-color: rgb(80, 80, 80);
-                    }""")
+                QPushButton[IsRunning=false]:hover
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }
 
-        self.btn_open_running_style = \
-            ("""QPushButton
-                    {
-                        color: rgb(255, 255, 255);
-                        background-color: rgb(204, 102, 51);
-                        border-style: solid;
-                        border-color: rgb(204, 102, 51);
-                        border-width: 6px;
-                    }
+                QPushButton[IsRunning=true]
+                {
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(0, 122, 204);
+                    border-style: solid;
+                    border-color: rgb(0, 122, 204);
+                    border-width: 0px 13px 0px 13px;
+                    qproperty-icon: none;
+                }
 
-                    QPushButton:pressed
-                    {
-                        background-color: rgb(204, 102, 51);
-                        border-color: rgb(204, 102, 51);
-                    }
+                QPushButton[IsRunning=true]:pressed
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }
 
-                    QPushButton:hover
-                    {
-                        background-color: rgb(204, 102, 51);
-                        border-color: rgb(204, 102, 51);
-                    }""")
+                QPushButton[IsRunning=true]:hover
+                {
+                    background-color: rgb(80, 80, 80);
+                    border-color: rgb(80, 80, 80);
+                }""")
 
-        self.btn_delete_running_style = \
-            ("""QPushButton
-                    {
-                        color: rgb(255, 255, 255);
-                        background-color: rgb(0, 122, 204);
-                        border-style: solid;
-                        border-color: rgb(0, 122, 204);
-                        border-width: 0px 13px 0px 13px;
-                        qproperty-icon: none;
-                    }
-
-                    QPushButton:hover
-                    {
-                        background-color: rgb(80, 80, 80);
-                        border-color: rgb(80, 80, 80);
-                    }""")
-
-        self.pids = []
+        self.instances_count = 0
         self.root_folder = root_folder
         self.version = version
         self.parent = parent
@@ -126,57 +129,60 @@ class B3dItemLayout(QHBoxLayout):
             self.btnOpen.setIcon(parent.fake_icon)
 
         self.btnOpen.setFont(QFont("MS Shell Dlg 2", 10))
-        self.btnDelete = QPushButton("")
-        self.btnDelete.setStyleSheet(self.btn_delete_style)
+        self.btnOpen.setCursor(QCursor(Qt.PointingHandCursor))
         self.btnOpen.setStyleSheet(self.btn_open_style)
+
+        self.btnOpen.setProperty('IsRunning', False)
+
+        self.btnDelete = QPushButton("")
         self.btnDelete.setFlat(True)
         self.btnDelete.setToolTip("Delete From Drive")
         self.btnDelete.setSizePolicy(
             QSizePolicy.Maximum, QSizePolicy.Preferred)
-        self.btnDelete.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        self.btnOpen.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        self.btnDelete.setCursor(QCursor(Qt.PointingHandCursor))
         self.btnDelete.clicked.connect(lambda: self.delete())
+        self.btnDelete.setStyleSheet(self.btn_delete_style)
+
+        self.btnDelete.setProperty('IsRunning', False)
 
         self.addWidget(self.btnOpen)
         self.addWidget(self.btnDelete)
 
-    async def wait_for_exit(self):
-        loop = asyncio.get_running_loop()
-
-        while True:
-            for pid in self.pids:
-                if not psutil.pid_exists(pid):
-                    self.pids.remove(pid)
-                    self.btnDelete.setText(str(len(self.pids)))
-
-                    if len(self.pids) == 0:
-                        self.btnOpen.setStyleSheet(self.btn_open_style)
-                        self.btnOpen.setEnabled(True)
-
-                        self.btnDelete.setStyleSheet(self.btn_delete_style)
-                        self.btnDelete.setIcon(self.parent.trash_icon)
-                        self.btnDelete.setText("")
-                        self.btnDelete.setToolTip("Delete From Drive")
-                        self.btnDelete.setEnabled(True)
-
-                        break
-
-            await asyncio.sleep(1)
-
     def open(self):
-        proc = subprocess.Popen(os.path.join(
+        process = QProcess(self)
+        process.started.connect(self.process_started)
+        process.finished.connect(self.process_finished)
+        process.start(os.path.join(
             self.root_folder, self.version, "blender.exe"))
-        self.pids.append(proc.pid)
-        self.btnDelete.setText(str(len(self.pids)))
 
-        if len(self.pids) == 1:
-            self.btnOpen.setStyleSheet(self.btn_open_running_style)
+    def process_started(self):
+        self.instances_count = self.instances_count + 1
+        self.btnDelete.setText(str(self.instances_count))
+
+        if self.instances_count == 1:
+            self.btnOpen.setProperty('IsRunning', True)
+            self.btnOpen.setStyle(self.btnOpen.style())
+
             self.btnDelete.setToolTip("Number of Running Instances")
-            self.btnDelete.setStyleSheet(self.btn_delete_running_style)
+            self.btnDelete.setProperty('IsRunning', True)
+            self.btnDelete.setStyle(self.btnDelete.style())
             self.btnDelete.setEnabled(False)
 
-            threading.Thread(target=lambda: asyncio.run(
-                self.wait_for_exit()), daemon=True).start()
+    def process_finished(self):
+        self.instances_count = self.instances_count - 1
+
+        if self.instances_count == 0:
+            self.btnOpen.setProperty('IsRunning', False)
+            self.btnOpen.setStyle(self.btnOpen.style())
+
+            self.btnDelete.setIcon(self.parent.trash_icon)
+            self.btnDelete.setText("")
+            self.btnDelete.setToolTip("Delete From Drive")
+            self.btnDelete.setEnabled(True)
+            self.btnDelete.setProperty('IsRunning', False)
+            self.btnDelete.setStyle(self.btnDelete.style())
+        else:
+            self.btnDelete.setText(str(self.instances_count))
 
     def delete(self):
         delete = QMessageBox.warning(
