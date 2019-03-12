548db4c38fb0729f71053ff9ca11ab4d93628675
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2019-03-12 08:39:49 +0400

    Fixed funcion typo.

--- main_window.py
+++ main_window.py
@@ -221,12 +221,13 @@ class B3dVersionMangerMainWindow(QMainWindow, main_window_design.Ui_MainWindow):
         self.set_progress_bar(0, "Downloading: %p%")
 
         self.build_loader = BuildLoader(
-            self.settings.value('root_folder'),
+            self,
+            # self.settings.value('root_folder'),
             self.uptodate_thread.download_url)
         self.build_loader.finished.connect(self.finished)
         self.build_loader.progress_changed.connect(self.set_progress_bar)
         self.build_loader.block_abortion.connect(
-            lambda: self.btnCancel.setEnabled(False))
+            lambda: self.btnCancel.setVisible(False))
         self.btnCancel.clicked.connect(self.build_loader.stop)
         self.build_loader.start()
 
