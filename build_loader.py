b053f3e57f30a46fd5ea3b444198adda04c98324
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2019-03-22 21:15:51 +0400

    Refactor. Fixed register .blend after renaming.

--- build_loader.py
+++ build_loader.py
@@ -9,54 +9,51 @@ from urllib.request import urlopen
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
-from version_layout import B3dItemLayout
-
 
 class BuildLoader(QThread):
     finished = pyqtSignal('PyQt_PyObject')
     block_abortion = pyqtSignal()
     progress_changed = pyqtSignal('PyQt_PyObject', 'PyQt_PyObject')
 
-    def __init__(self, parent, download_url):
+    def __init__(self, parent, url):
         QThread.__init__(self)
-        self.download_url = download_url
+        self.url = url
         self.parent = parent
         self.root_folder = self.parent.settings.value('root_folder')
         self.is_running = False
 
     def run(self):
         self.is_running = True
-        blender_zip = urlopen(self.download_url)
+        blender_zip = urlopen(self.url)
         size = blender_zip.info()['Content-Length']
         temp_path = os.path.join(self.root_folder, "temp")
-        download_path = os.path.join(
-            temp_path, self.download_url.split('/', -1)[-1])
+        path = os.path.join(temp_path, self.url.split('/', -1)[-1])
 
         # Create temp directory
         if not os.path.isdir(temp_path):
             os.makedirs(temp_path)
 
         # Download
-        with open(download_path, 'wb') as self.f:
+        with open(path, 'wb') as file:
             while True:
                 chunk = blender_zip.read(16 * 1024)
 
                 if not chunk:
                     break
 
-                self.f.write(chunk)
+                file.write(chunk)
                 self.progress_changed.emit(
-                    os.stat(download_path).st_size / int(size), "Downloading: %p%")
+                    os.stat(path).st_size / int(size), "Downloading: %p%")
 
                 if not self.is_running:
-                    self.f.close()
+                    file.close()
                     if os.path.isdir(temp_path):
                         shutil.rmtree(temp_path)
                     self.finished.emit(None)
                     return
 
         # Extract
-        zf = zipfile.ZipFile(download_path)
+        zf = zipfile.ZipFile(path)
         version = zf.infolist()[0].filename.split('/')[0]
         uncompress_size = sum((file.file_size for file in zf.infolist()))
         extracted_size = 0
@@ -77,24 +74,18 @@ class BuildLoader(QThread):
 
         zf.close()
         self.block_abortion.emit()
+        self.progress_changed.emit(0, "Finishing...")
 
         # Delete temp folder
-        self.progress_changed.emit(0, "Deleting temporary files...")
         if os.path.isdir(temp_path):
             shutil.rmtree(temp_path)
 
-        # Register .blend extension
+        # Get blender version info
         b3d_exe = os.path.join(self.root_folder, version, "blender.exe")
 
-        if self.parent.settings.value('is_register_blend', type=bool):
-            self.progress_changed.emit(0, "Registering .blend extension...")
-            subprocess.call([b3d_exe, "-r"],
-                            creationflags=subprocess.CREATE_NO_WINDOW)
-
-        # Get blender version info
-        self.progress_changed.emit(0, "Finishing...")
         info = subprocess.check_output(
-            [b3d_exe, "-v"], creationflags=CREATE_NO_WINDOW, shell=True, stderr=DEVNULL, stdin=DEVNULL).decode('UTF-8')
+            [b3d_exe, "-v"], creationflags=CREATE_NO_WINDOW, shell=True,
+            stderr=DEVNULL, stdin=DEVNULL).decode('UTF-8')
 
         ctime = re.search("build commit time: " + "(.*)", info)[1].rstrip()
         cdate = re.search("build commit date: " + "(.*)", info)[1].rstrip()
@@ -105,6 +96,13 @@ class BuildLoader(QThread):
         nice_name = "Git-%s-%s" % (git, time.strftime("%d-%b-%H-%M", strptime))
         os.rename(os.path.join(self.root_folder, version),
                   os.path.join(self.root_folder, nice_name))
+
+        # Register .blend extension
+        b3d_exe = os.path.join(self.root_folder, nice_name, "blender.exe")
+
+        if self.parent.settings.value('is_register_blend', type=bool):
+            subprocess.call([b3d_exe, "-r"],
+                            creationflags=subprocess.CREATE_NO_WINDOW)
 
         self.finished.emit(nice_name)
 
