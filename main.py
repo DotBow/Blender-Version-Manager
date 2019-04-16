1c9e5de2ee8e6bea1a4485818a1b4ae972bcfdf1
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2019-04-16 11:05:21 +0400

    Update version to 1.4.1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from main_window import B3dVersionMangerMainWindow
 
 def main():
     QApplication.setApplicationName("Blender Version Manager")
-    QApplication.setApplicationVersion("1.4.0")
+    QApplication.setApplicationVersion("1.4.1")
 
     app = QApplication(sys.argv)
     window = B3dVersionMangerMainWindow(app)
